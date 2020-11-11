a47898d8809878a987c6b2b658c2544ed78ff84a
Goncalo Marques <goncalomarques9@hotmail.com>
2020-11-11 17:01:40 +0000

    done

--- routes/auth.js
+++ routes/auth.js
@@ -15,15 +15,65 @@ router.get('/signup', async (req, res) => {
 })
 
 router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password ){
+      res.render('auth/login', {errorMessage: 'Please enter username and password'});
+      return;
+    }
+    
     try {
-        const { username, password } = req.body;
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hash = bcrypt.hashSync(password, salt);
-        await User.create({ username, password: hash });
-        res.redirect('/user/list')
+        const checkIfUserIsUnique = await User.findOne({ 'username': username });
+
+        if(!checkIfUserIsUnique){
+            const { username, password } = req.body;
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hash = bcrypt.hashSync(password, salt);
+            await User.create({ username, password: hash });
+            res.redirect('/'); 
+        } else {
+            res.render('auth/sign-up', { errorMessage: 'The username already exists' });
+        }
     } catch (error) {
         console.log(error)
     }
+})
+
+router.get('/login', async (req, res) => {
+    try {
+      res.render('auth/login')
+    } catch (error) {
+      console.log(error)
+    }
+})
+
+router.post('/login', async (req, res) => {
+
+    const { username, password } = req.body;
+
+    if(!username || !password ){
+      res.render('auth/login', {errorMessage: 'Please enter username and password'});
+      return;
+    }
+
+  try {
+    const foundUser = await User.findOne({'username': username});
+      
+    if (!foundUser) {
+      res.render('auth/login', {errorMessage: 'Invalid Login'});
+      return;
+    }
+    
+    if(bcrypt.compareSync(password, foundUser.password)){
+      req.session.currentUser = foundUser;
+      res.redirect('/');
+    } else {
+      res.render('auth/login'), {errorMessage: 'Invalid login'}
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.model')
+
+function requireLogin (req, res, next) {
+    if(req.session.currentUser){
+      next();
+    } else {
+      res.redirect('/login');
+    };
+};
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index', {user: req.session.currentUser})
+});
 
 router.get('/user/list', async (req, res) => {
     try {
@@ -11,6 +22,14 @@ router.get('/user/list', async (req, res) => {
     } catch (error) {
         res.render('error')
     }
+});
+
+router.get('/private', requireLogin, (req, res) => {
+    res.render('private')
+})
+
+router.get('/main', requireLogin, (req, res) => {
+    res.render('main')
 })
 
 module.exports = router;
